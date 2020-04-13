77fbbae813601714b3ed5f6da902d3e58f851a7a
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-04-13 00:45:04 -0500

    intentando hacer lista en Angular

---
+++ src/app/pages/content-list/content-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { NbCardModule, NbIconModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { ContentListRoutingModule, routedComponents } from './content-list-routing.module';
+
+@NgModule({
+  imports: [
+    NbCardModule,
+    NbTreeGridModule,
+    NbIconModule,
+    NbInputModule,
+    ThemeModule,
+    ContentListRoutingModule,
+  ],
+  declarations: [
+    ...routedComponents,
+  ],
+})
+export class TablesModule { }
