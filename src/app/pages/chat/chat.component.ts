683032aaa377c8baef768a86175f952c1a1b797a
Dianis Caro López <dmcaro@uniandes.edu.co>
2020-04-25 23:04:33 -0500

    Chat y nuevos módulos en app-module

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from '@angular/core';
+
+import { ChatService } from './chat.service';
 
 @Component({
-  selector: "app-chat",
-  templateUrl: "./chat.component.html",
-  styleUrls: ["./chat.component.scss"],
+  selector: 'ngx-chat',
+  templateUrl: 'chat.component.html',
+  styleUrls: ['chat.component.scss'],
+  providers: [ ChatService ],
 })
-export class ChatComponent implements OnInit {
-  constructor() {}
+export class ChatComponent {
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  messages: any[];
+
+  constructor(protected chatService: ChatService) {
+    this.messages = this.chatService.loadMessages();
+  }
+
+  sendMessage(event: any) {
+    const files = !event.files ? [] : event.files.map((file) => {
+      return {
+        url: file.src,
+        type: file.type,
+        icon: 'nb-compose',
+      };
+    });
+
+    this.messages.push({
+      text: event.message,
+      date: new Date(),
+      reply: true,
+      type: files.length ? 'file' : 'text',
+      files: files,
+      user: {
+        name: 'Nicolás P.',
+        avatar: 'https://i.gifer.com/no.gif',
+      },
+    });
+    const botReply = this.chatService.reply(event.message);
+    if (botReply) {
+      setTimeout(() => { this.messages.push(botReply); }, 500);
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthService {
-  url = "http://127.0.0.1:3000/api/v1/";
+  url = "http://127.0.0.1/api/v1/";
   token = localStorage.getItem("jwtGuruhApp");
   constructor(private http: HttpClient) {}
 
